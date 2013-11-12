64e17dec8628ffffddac28e578e5c23ab900862f
Paul Reioux <reioux@gmail.com>
2013-11-11 19:52:51 -0600

    kernel/power/powersuspend: default to userspace for now
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- kernel/power/powersuspend.c
+++ kernel/power/powersuspend.c
@@ -247,8 +247,7 @@ static struct attribute_group power_suspend_attr_group =
 
 static struct kobject *power_suspend_kobj;
 
-// ------------------------------------------ sysfs interface ------------------------------------------
-
+// ------------------ sysfs interface -----------------------
 static int __init power_suspend_init(void)
 {
 
@@ -276,7 +275,7 @@ static int __init power_suspend_init(void)
 		return -ENOMEM;
 	}
 
-	mode = POWER_SUSPEND_KERNEL; // Yank555.lu : Default to kernel mode
+	mode = POWER_SUSPEND_USERSPACE;
 
 	return 0;
 }
