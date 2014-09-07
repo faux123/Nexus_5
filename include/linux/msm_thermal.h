8a12801b55e96d188b95947b6dd5751194b7ef14
Paul Reioux <reioux@gmail.com>
2014-09-07 13:34:18 -0700

    intelli-thermal v2: initial adaptation
    Intellithermal 2.0 is based on the latest Qualcomm thermal driver adapted for
    in-kernel use and control.  Newer MSM8974+ chipsets should use this driver
    going forward
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -47,10 +47,16 @@ struct msm_thermal_data {
 	int32_t vdd_rstr_temp_hyst_degC;
 	int32_t psm_temp_degC;
 	int32_t psm_temp_hyst_degC;
+#ifdef CONFIG_INTELLI_THERMAL_V2
+	int32_t ocr_temp_degC;
+	int32_t ocr_temp_hyst_degC;
+#endif
 };
 #endif
 
-#if defined(CONFIG_THERMAL_MONITOR) || defined(CONFIG_INTELLI_THERMAL)
+#if defined(CONFIG_THERMAL_MONITOR) ||\
+    defined(CONFIG_INTELLI_THERMAL) ||\
+    defined(CONFIG_INTELLI_THERMAL_V2)
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 extern int msm_thermal_device_init(void);
 extern int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq,
