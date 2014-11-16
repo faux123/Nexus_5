33ae17fed52d7fdb9a8b74ecad8934bab769d5e4
Paul Reioux <reioux@gmail.com>
2014-11-15 20:19:43 -0800

    drivers/thermal/Kconfig: fix merge derp
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -40,7 +40,6 @@ struct msm_thermal_data {
 #endif
 };
 
-#ifdef CONFIG_THERMAL_MONITOR
 #if defined(CONFIG_THERMAL_MONITOR) ||\
     defined(CONFIG_INTELLI_THERMAL_V2)
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
